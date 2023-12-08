d740882222b1161a763ce0a7a03ac304eb48af0b
MS MS <supurpisces310@msn.com>
2023-12-08 20:59:43 +0000

    Completed most common character lab

--- src/main/java/MostCommonCharacter.java
+++ src/main/java/MostCommonCharacter.java
@@ -1,4 +1,9 @@
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 public class MostCommonCharacter {
     /**
      * Find the most common character in str.
@@ -8,6 +13,25 @@ public class MostCommonCharacter {
      * @return the most common character within str.
      */
     public char recurringChar(String str) {
-        return ' ';
+        List<Character> chars = new ArrayList<>(); 
+        Map<Character, Integer> feq = new HashMap<Character, Integer>();
+        char letter = '\0'; 
+             for (char ch : str.toCharArray()) { 
+                 chars.add(ch); 
+             } 
+             for (Character x : chars){
+                feq.put(x, Collections.frequency(chars, x));
+              }
+        int max = (Collections.max(feq.values()));
+        
+        for(Map.Entry<Character, Integer> c: feq.entrySet()) {
+               if(c.getValue() == max) {
+                   letter = c.getKey();
+               }
+        }
+         return letter;
     }
+
+
+
 }
